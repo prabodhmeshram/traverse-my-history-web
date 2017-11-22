9cbd00b2c1cb489de223fbe01e6c83e3ea334dc4
Prabodh Meshram <prabodh.meshram7@gmail.com>
2017-11-22 22:47:42 +0530

    Refactoring App

---
+++ src/containers/Dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class Dashboard extends Component{
+
+    render(){
+        return(
+            <div>
+                <header>
+                    <h1>Web App for Traverse My History</h1>
+                </header>
+            </div>
+        );
+    }
+}
